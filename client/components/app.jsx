df769e1105f1226694f7c4d4166843b86265068f
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-06-07 20:44:28 +0000

    added getAvgGrade method to App component.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { grades: [] };
+    this.getAverageGrade = this.getAverageGrade.bind(this);
   }
 
   componentDidMount() {
@@ -19,6 +20,19 @@ class App extends React.Component {
       .catch(err => {
         console.error('error:', err);
       });
+  }
+
+  getAverageGrade() {
+    let gradesTotal = null;
+    const gradesCount = this.state.grades.length;
+
+    for (let i = 0; i < this.state.grades.length; i++) {
+      gradesTotal += this.state.grades[i].grade;
+    }
+
+    const avgGrade = Math.round(gradesTotal / gradesCount);
+
+    return avgGrade;
 
   }
 
